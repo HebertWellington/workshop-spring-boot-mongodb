5c3f7fedc155ae38fed3cab08b50f7775287d49a
HebertWellington <hebertwellington@gmail.com>
2022-01-28 20:00:05 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/hebertwellington/workshopmongo/config/Instantiation.java
+++ src/main/java/com/hebertwellington/workshopmongo/config/Instantiation.java
@@ -3,7 +3,6 @@ package com.hebertwellington.workshopmongo.config;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
