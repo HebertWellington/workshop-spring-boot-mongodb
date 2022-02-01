4806af7c44a89084effd0af0f684d2bb4f9dbfcb
HebertWellington <hebertwellington@gmail.com>
2022-02-01 13:00:37 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/hebertwellington/workshopmongo/domain/Post.java
+++ src/main/java/com/hebertwellington/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.hebertwellington.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.hebertwellington.workshopmongo.dto.AuthorDTO;
+import com.hebertwellington.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,19 +21,21 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private AuthorDTO autor;
+	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
 	}
 
-	public Post(String id, Date date, String title, String body, AuthorDTO autor) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
 		this.title = title;
 		this.body = body;
-		this.autor = autor;
+		this.author = author;
 	}
 
 	public String getId() {
@@ -66,11 +71,19 @@ public class Post implements Serializable {
 	}
 
 	public AuthorDTO getAutor() {
-		return autor;
+		return author;
 	}
 
 	public void setAutor(AuthorDTO autor) {
-		this.autor = autor;
+		this.author = autor;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 	
 	@Override
